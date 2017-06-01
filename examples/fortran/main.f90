bca1ff0f93849bee92b91469dc7d4ddc80057a33
Willi Kappler <willi.kappler@uni-tuebingen.de>
2017-06-01 17:03:04 +0200

    first nom parser for fortran

--- examples/fortran/main.f90
+++ examples/fortran/main.f90
@@ -1,8 +1,11 @@
-program test_comment_unit
+! Test program for comment_units
+program test_comment_units1
   use m_util
 
+  ! No implicit variables
   implicit none
 
+  ! For now just real (double) variables:
   real(8) :: velocity
   real(8) :: length, duration
 
--- src/language_fortran.rs
+++ src/language_fortran.rs
@@ -1,7 +1,54 @@
 // External module
 use nom;
 
+// Grammer for Fortran code
+named!(fortran_code<&str, &str>, alt!(program | module));
 
+named!(program<&str, &str>, do_parse!(
+    ws!(tag!("program")) >>
+    ws!(identifier) >>
+
+    statements >>
+
+    ws!(tag!("end program")) >>
+    ws!(identifier)
+));
+
+named!(module<&str, &str>, do_parse!(
+    ws!(tag!("module")) >>
+    ws!(identifier) >>
+
+    statements >>
+
+    ws!(tag!("end module")) >>
+    ws!(identifier)
+));
+
+named!(identifier<&str, &str>, do_parse!(
+    alpha >>
+    many0!(alt!(alplanumeric | tag!("_")))
+));
+
+named!(statement<&str, &str>, do_parse!(
+    many0!(stmt_use) >>
+    many0!(stmt_implicit) >>
+    many0!(declaration)
+));
+
+named!(stmt_use<&str, &str>, do_parse!(
+    ws!(tag!("use")) >>
+    ws!(identifier)
+));
+
+named!(stmt_implicit<&str, &str>, do_parse!(
+    ws!(tag!("implicit none"))
+));
+
+named!(declaration<&str, &str>, do_parse!(
+    ws!(type) >>
+    ws!(tag!("::")) >>
+    ws!(identifier)
+));
 
 pub fn process_language_fortran(file_name: &str) {
     println!("processing fortran source file: '{}'", file_name);
