ce5bfc07a4e3ef5d0a97492a9acd8ccd6c2f238c
Willi Kappler <willi.kappler@uni-tuebingen.de>
2017-06-06 19:45:19 +0200

    use nom again

--- src/language_fortran.rs
+++ src/language_fortran.rs
@@ -1,46 +1,12 @@
 use std::fs::File;
 use std::io::Read;
 
+use nom::{alphanumeric};
+
 #[derive(PartialEq, Debug)]
 enum FortranTokenType {
-    Comment(String),         // Simgle line comment with !
-    StringLiteral(String),   // Can be in " or '
-    Boolean(bool),           // .true. or .false.
-    IntegerI64(i64),         // 12, +67, 88
-    IntegerU64(u64),         // -12, -67, -88
-    FloatF64(f64),           // 1.4, -86.345, 2.6e3
-    Identifier(String),      // distance, particle_id
-    OperatorPlus,       // +
-    OperatorMinus,      // -
-    OperatorMul,        // *
-    OperatorDiv,        // /
-    OperatorEq,         // ==, .eq.
-    OperatorNeq,        // /=, .neq.
-    OperatorLT,         // <=, .leq.
-    OperatorGT,         // >=, .geq.
-    OperatorStruct,     // %
-    Assign,             // =
-    LParen,             // (
-    RParen,             // )
-    LBracket,           // [
-    RBracket,           // ]
-    Comma,              // ,
-    ColonColon,         // ::
-
-    // All keywords:
-    KeywordEnd,
-    KeywordProgram,
-    KeywordUse,
-    KeywordImplicit,
-    KeywordNone,
-    KeywordReal,
-    KeywordPrint,
-    KeywordIf,
-    KeywordThen,
-    KeywordElse,
-    KeywordDo,
-    KeywordSubroutine,
-
+    Comment(String),            // Simgle line comment with !
+    Itentifier(String),         // distance, particle_id
 }
 
 #[derive(PartialEq, Debug)]
@@ -50,14 +16,77 @@ struct Token {
     token: FortranTokenType
 }
 
-#[derive(PartialEq, Debug)]
-enum ParseMode {
-    NoMode,
-    StringLiteral,
-    Identifier,
-    Number,
-    Comment,
-}
+named!(parse_comment<&str, Option<FortranTokenType>>, ws!(opt!(
+    do_parse!(
+        tag!("!") >>
+        content: take_while!(call!(|c| c != '\n')) >>
+        (FortranTokenType::Comment(content.to_string()))
+    ))));
+
+named!(parse_comment_line<&str, FortranTokenType>, ws!(
+    do_parse!(
+        tag!("!") >>
+        content: take_while!(call!(|c| c != '\n')) >>
+        (FortranTokenType::Comment(content.to_string()))
+    )));
+
+named!(parse_many_comment_lines<&str, Vec<FortranTokenType> >, many0!(parse_comment_line));
+
+// fn parse_identifier(input: &str) -> IResult<&str, String> {
+//     if input.len() == 0 {
+//         IResult::Incomplete(Needed::Unknown)
+//     } else {
+//         if !input[0].is_alphabetic() {
+//             IResult::Error
+//         } else {
+//             IResult::Done
+//         }
+//     }
+// }
+
+named!(parse_identifier<&str, String>, do_parse!(
+    values: many1!(alt!(alphanumeric | tag!("_"))) >>
+    (values.join(""))
+));
+
+named!(parse_program<&str, Vec<FortranTokenType> >, do_parse!(
+    ws!(tag!("program")) >>
+    ws!(parse_identifier) >>
+    // parse_use_statements >>
+    // parse_implicit_statements >>
+    // variable_decls: parse_variable_declarations >>
+    // statements: parse_statements >>
+    // parse_many_comment_lines >>
+    ws!(tag!("end")) >>
+    ws!(tag!("program")) >>
+    ws!(parse_identifier) >>
+    parse_comment >>
+    parse_many_comment_lines >>
+    //(variable_decls.extend(&statements))
+
+    parse_comment >>
+    (Vec::new())
+));
+
+named!(parse_module<&str, Vec<FortranTokenType> >, do_parse!(
+    ws!(tag!("program")) >>
+    ws!(parse_identifier) >>
+    parse_comment >>
+    // TODO: add function and subroutine
+    ws!(tag!("end")) >>
+    ws!(tag!("program")) >>
+    ws!(parse_identifier) >>
+    parse_comment >>
+    parse_many_comment_lines >>
+    (Vec::new())
+));
+
+named!(parse_code<&str, Vec<FortranTokenType> >, do_parse!(
+    parse_many_comment_lines >>
+    actual_code: alt!(parse_program | parse_module) >>
+    (actual_code)
+));
+
 
 pub fn process_language_fortran(file_name: &str) {
     println!("processing fortran source file: '{}'", file_name);
@@ -68,103 +97,7 @@ pub fn process_language_fortran(file_name: &str) {
 
     f.read_to_string(&mut buffer).expect("unable to read to string");
 
-    process_fortran_source_code(&mut buffer)
-}
+    let result = parse_code(&buffer);
+    println!("result: {:?}", result);
 
-
-fn process_fortran_source_code(buffer: &str) {
-
-    for (line_number, line) in (1..).zip(buffer.lines()) {
-        println!("{}: {}", line_number, line);
-        let tokens = split_into_fortran_tokens(line, line_number);
-        println!("token: {:?}", tokens);
-    }
-}
-
-fn split_into_fortran_tokens(line: &str, line_number: u32) -> Vec<Token> {
-    let mut result = Vec::<Token>::new();
-    let mut mode = ParseMode::NoMode;
-    let mut content = String::new();
-    let mut token_column: u32 = 1;
-
-    for (column, c) in (1..).zip(line.chars()) {
-        match mode {
-            ParseMode::NoMode => {
-                match c {
-                    ' ' | '\n' => {},
-                    '!' => {
-                        mode = ParseMode::Comment;
-                        token_column = column;
-                    },
-                    'a' ... 'z' | 'A' ... 'Z' => {
-                        mode = ParseMode::Identifier;
-                        token_column = column;
-                        content.push(c)
-                    }
-                    '0' ... '9' | '+' | '-' => {
-                        mode = ParseMode::Number;
-                        token_column = column;
-                        content.push(c)
-                    }
-                    '"' | '\'' => {
-                        mode = ParseMode::StringLiteral;
-                        token_column = column;
-                    },
-                    _ => {} // TODO
-                }
-            },
-            ParseMode::StringLiteral => {
-
-            },
-            ParseMode::Identifier => {
-                match c {
-                    'a' ... 'z' | 'A' ... 'Z' | '0' ... '9' | '_' => {
-                        content.push(c)
-                    },
-                    ' ' | '\n' => {
-                        result.push(Token{
-                            line_number: line_number,
-                            column: token_column,
-                            token: FortranTokenType::Identifier(content.clone())});
-                        content = String::new();
-                        mode = ParseMode::NoMode
-                    },
-                    _ => { // TODO
-                        mode = ParseMode::NoMode
-                    }
-                }
-            },
-            ParseMode::Number => {
-
-            },
-            ParseMode::Comment => {
-                content.push(c)
-            }
-        }
-    }
-
-    match mode {
-        ParseMode::NoMode => {
-        },
-        ParseMode::StringLiteral => {
-
-        },
-        ParseMode::Identifier => {
-            result.push(Token{
-                line_number: line_number,
-                column: token_column,
-                token: FortranTokenType::Identifier(content)});
-        },
-        ParseMode::Number => {
-
-        },
-        ParseMode::Comment => {
-            result.push(Token{
-                line_number: line_number,
-                column: token_column,
-                token: FortranTokenType::Comment(content)})
-        }
-    }
-
-    result
 }
